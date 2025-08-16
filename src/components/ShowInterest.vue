f8ee16487d8dbb28bb1aa92c2f5349d6df722ec7
Reinier <reinier7175@gmail.com>
2025-08-16 20:16:56 +0200

    fcd

--- src/components/ShowInterest.vue
+++ src/components/ShowInterest.vue
@@ -11,8 +11,8 @@ import Button from "./Button.vue";
   </h1>
   <p class="description">
     Click the button below to access a <strong>private Google Form</strong> where you can express your interest in joining the fund.
-    Completing the form does <strong>not commit you</strong>, but it ensures I can reach out if a rare opening becomes available.
-    Your interest is greatly valued, and I look forward to the possibility of <strong>welcoming you into this select circle</strong>.
+    Completing the form does <strong>not commit you</strong> and also does <strong>not guarantee</strong> you a possible spot into the fund.
+    Your interest is greatly valued, and I look forward to the possibility of welcoming you into this <strong>select circle</strong>.
   </p>
 
   <Button></Button>
--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,3 @@
 export const TRANSACTION_COST_NOTE =
-    '*Transaction costs of 0.05% included, performance since inception, updated monthly.'
+    '*Transaction costs of 0.05% included, performance since inception, updated monthly. ' +
+    'Past performance is not indicative of future results.'
