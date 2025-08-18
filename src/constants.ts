e5a28945e7d75e6eff46645a90d07a7d3b04c5dd
Reinier Schep <reinier7175@gmail.com>
2025-08-18 20:53:10 +0200

    k

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 export const TRANSACTION_COST_NOTE =
-    '*Transaction costs of 0.05% included, performance since inception, updated monthly. ' +
+    'Returns include 0.03% cost per trade, performance since inception, updated monthly. ' +
     'Past performance is not indicative of future results.'
