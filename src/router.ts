dff7b0901b48857ab721a88b170ecc74b209a7c8
Reinier <reinier7175@gmail.com>
2025-11-19 17:32:05 +0100

    github pages fix

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from './pages/Home.vue'
 import Old from './pages/Old.vue'
 
 export const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(), // <-- use hash mode
     routes: [
         { path: '/', component: Home },
         { path: '/n0v2q92p9y825v2948p75a279ov34bo6av2o97', component: Old },
--- src/stripped_components/ShowInterestStripped.vue
+++ src/stripped_components/ShowInterestStripped.vue
@@ -10,14 +10,11 @@ import Button from "../components/Button.vue";
     Show your <span class="highlight">interest</span>
   </h1>
   <p class="description">
-    <p>
       Click the button below to access a private Google Form where you can express your interest in learning more about the fund.
       Completing the form does <strong>not commit you</strong> and also does <strong>not guarantee</strong> any participation.
       Your interest is greatly valued, and I look forward to the possibility of sharing more information with you. <br><br>
 
       By filling out the form, you can receive additional details about the strategy, methodology, and relevant performance information for informational purposes only.
-    </p>
-
   </p>
 
   <Button></Button>
